7927c61b1d0da6397b38616270893c268642f71e
iLexiconn <ilexiconn@gmail.com>
2014-05-11 20:22:53 +0200

    I really love my updates ;_;

--- src/main/java/com/ilexiconn/fossils/Util.java
+++ src/main/java/com/ilexiconn/fossils/Util.java
@@ -1,6 +1,10 @@
 package com.ilexiconn.fossils;
 
+import com.ilexiconn.fossils.proxy.ServerProxy;
+import cpw.mods.fml.common.SidedProxy;
+
 public class Util
 {
-
+    @SidedProxy(clientSide = "com.ilexiconn.fossils.proxy.ClientProxy", serverSide = "com.ilexiconn.fossils.proxy.ServerProxy")
+    public static ServerProxy proxy;
 }
---
+++ src/main/java/com/ilexiconn/fossils/proxy/ClientProxy.java
@@ -0,0 +1,14 @@
+package com.ilexiconn.fossils.proxy;
+
+public class ClientProxy extends ServerProxy
+{
+    public void renderEntity()
+    {
+
+    }
+
+    public void renderTileEntity()
+    {
+
+    }
+}
