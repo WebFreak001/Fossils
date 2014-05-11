52947ea5d490afc9ba8f7c6211f8e5edb2d2ccad
iLexiconn <ilexiconn@gmail.com>
2014-05-11 20:18:25 +0200

    I really love my updates ;_;

--- src/main/java/com/ilexiconn/fossils/Main.java
+++ src/main/java/com/ilexiconn/fossils/Main.java
@@ -1,6 +1,17 @@
 package com.ilexiconn.fossils;
 
-public class Main
-{
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
+@Mod(modid = "fossils", name = "Fossils and Archaeology mod: Jurassic Park Edition", version = "1.0.1")
+public class Main extends Util
+{
+    @Mod.Instance("fossils")
+    public static Main instance;
+
+    @Mod.EventHandler
+    public void init(FMLPreInitializationEvent event)
+    {
+
+    }
 }
