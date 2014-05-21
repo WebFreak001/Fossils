47c8a29d1169cdf53a9eafdfca08e1332a5d4ce6
iLexiconn <ilexiconn@gmail.com>
2014-05-21 15:05:16 +0200

    Wow.

--- src/main/java/assets/fossils/textures/models/blocks/ModelCultivate.java
+++ src/main/java/assets/fossils/textures/models/blocks/ModelCultivate.java
@@ -1,4 +1,4 @@
-package com.fossils.models.blocks;
+package assets.fossils.textures.models.blocks;
 
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
--- src/main/java/com/fossils/ilexiconn/block/tileentity/TileCultivate.java
+++
@@ -1,8 +0,0 @@
-package com.fossils.ilexiconn.block.tileentity;
-
-import net.minecraft.tileentity.TileEntity;
-
-public class TileCultivate extends TileEntity
-{
-
-}
