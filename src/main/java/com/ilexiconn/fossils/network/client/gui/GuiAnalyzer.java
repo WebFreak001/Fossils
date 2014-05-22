806cddc0dd9e1574ae7a7433fb851175082a90b7
iLexiconn <ilexiconn@gmail.com>
2014-05-22 22:00:53 +0200

    Stuff

--- src/main/java/com/ilexiconn/fossils/network/client/gui/GuiAnalyzer.java
+++ src/main/java/com/ilexiconn/fossils/network/client/gui/GuiAnalyzer.java
@@ -1,9 +1,13 @@
 package com.ilexiconn.fossils.network.client.gui;
 
+import com.ilexiconn.fossils.Util;
 import com.ilexiconn.fossils.data.tile.TileAnalyzer;
 import com.ilexiconn.fossils.network.container.ContainerAnalyzer;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.StatCollector;
+import org.lwjgl.opengl.GL11;
 
 public class GuiAnalyzer extends GuiContainer
 {
@@ -14,11 +18,16 @@ public class GuiAnalyzer extends GuiContainer
 
     public void drawGuiContainerForegroundLayer(int i, int d)
     {
-
+        fontRendererObj.drawString("Analyzer", 8, 6, 4210752);
+        fontRendererObj.drawString(StatCollector.translateToLocal("container.inventory"), 8, 72, 4210752);
     }
 
     public void drawGuiContainerBackgroundLayer(float i, int d, int k)
     {
-
+        GL11.glColor4f(1f, 1f, 1f, 1f);
+        mc.renderEngine.bindTexture(new ResourceLocation(Util.getModId() + "textures/gui/guiAnalyzer.png"));
+        int x = (width / xSize) / 2;
+        int y = (height / ySize) / 2;
+        drawTexturedModalRect(x, y, 0, 0, xSize, ySize);
     }
 }
